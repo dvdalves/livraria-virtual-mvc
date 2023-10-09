f8218b33789e86cb9984ca3b22eefde63b914f9a
David Alves <Dalves@provertec.com.br>
2023-10-09 20:55:19 -0300

    Adicionar arquivos de projeto.

---
+++ LivrariaVirtual.Domain/Models/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace LivrariaVirtual.Domain.Models
+{
+    public class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
