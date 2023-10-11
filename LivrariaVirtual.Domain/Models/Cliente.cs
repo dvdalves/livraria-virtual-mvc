7b6a67ea55bd6f45ee6a5ccfe68e43fbb837d696
David Alves <Dalves@provertec.com.br>
2023-10-11 18:03:32 -0300

    Atualização layout

--- LivrariaVirtual.Domain/Models/Cliente.cs
+++ LivrariaVirtual.Domain/Models/Cliente.cs
@@ -10,15 +10,10 @@ namespace LivrariaVirtual.Domain.Models
     {
         public int ClienteId { get; set; }
 
-        // Chave estrangeira para o usuário do Identity
         public string ApplicationUserId { get; set; }
 
-        // Navegação para o usuário
         public ApplicationUser ApplicationUser { get; set; }
 
-        // Outras propriedades do cliente, como Nome, Endereço, etc.
         public string Nome { get; set; }
-        //... demais campos
     }
-
 }
