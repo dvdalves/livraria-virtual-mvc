eb92aad85351b3ca3ea56ca858d32f987d9ffecc
David Alves <david.a0595@gmail.com>
2023-11-08 20:54:02 -0300

    Retirando as referências não usadas

--- LivrariaVirtual.Domain/Models/Produto.cs
+++ LivrariaVirtual.Domain/Models/Produto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LivrariaVirtual.Domain.Models
+﻿namespace LivrariaVirtual.Domain.Models
 {
     public class Produto : Entity
     {
