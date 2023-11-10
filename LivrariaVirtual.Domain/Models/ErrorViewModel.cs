c00cd1acfee226fb9a3fc9fbe78400a351c945e5
David Alves <david.a0595@gmail.com>
2023-11-10 19:19:48 -0300

    Retirando classes não usadas

--- LivrariaVirtual.Domain/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LivrariaVirtual.Domain.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- LivrariaVirtual/Controllers/HomeController.cs
+++ LivrariaVirtual/Controllers/HomeController.cs
@@ -27,12 +27,5 @@ namespace LivrariaVirtual.Controllers
         {
             return View();
         }
-
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
