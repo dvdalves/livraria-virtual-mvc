eef6307710992ecc855ca4dd5a946e1fee246426
David Alves <109628134+dvdalves@users.noreply.github.com>
2023-11-22 23:34:32 -0300

    CRUD Livros

---
+++ LivrariaVirtual.Domain/Models/Livro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivrariaVirtual.Domain.Models
+{
+    public class Livro : Entity
+    {
+        public string Titulo { get; set; }
+        public string Tipo { get; set; }
+        public DateOnly Data { get; set; }
+    }
+}
