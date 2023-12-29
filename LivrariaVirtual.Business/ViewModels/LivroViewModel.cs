fc9cd974772bd526db94113eaee7fd61b4bed0d2
David Alves <Dalves@provertec.com.br>
2023-12-29 14:17:10 -0300

    Adicionar propriedade calculada DataFormatada à ViewModel

--- LivrariaVirtual.Business/ViewModels/LivroViewModel.cs
+++ LivrariaVirtual.Business/ViewModels/LivroViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace LivrariaVirtual.Business.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LivrariaVirtual.Business.ViewModels
 {
     public class LivroViewModel : BaseViewModel
     {
         public string? Titulo { get; set; }
         public string? Genero { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime? Data { get; set; }
+
         public string? Autor { get; set; }
         public decimal? Preco { get; set; }
         public string? Idioma { get; set; }
